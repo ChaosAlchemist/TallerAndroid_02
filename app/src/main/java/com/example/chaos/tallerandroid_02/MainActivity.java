834f49607cfa32b1dcef7c6c20138db04639b54f
ChaosAlchemist <jrcortest1@gmail.com>
2018-04-04 20:51:58 -0300

    Interfaz gráfica y modulos añadidos.

--- app/src/main/java/com/example/chaos/tallerandroid_02/MainActivity.java
+++ app/src/main/java/com/example/chaos/tallerandroid_02/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.chaos.tallerandroid_02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Sistema Notas");
+    }
+
+    public void btnIngresarMenuOnClick(View v){
+        // Verificar usuario y contraseña
+        // !!
+
+        // Redirigir al menú si está OK
+        Intent i = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(i);
+
     }
 }
--- app/src/main/java/com/example/chaos/tallerandroid_02/MenuActivity.java
+++ app/src/main/java/com/example/chaos/tallerandroid_02/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.chaos.tallerandroid_02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         setTitle("Menú Notas");
+    }
+
+    public void btnVerEstadisticasOnClick(View v){
+        // Redirigir a Ver Estadísticas
+        Intent i = new Intent(MenuActivity.this, VerEstadisticasActivity.class);
+        startActivity(i);
+
     }
 }
